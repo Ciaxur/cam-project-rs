a23407f249bed5ce21ed0d451debf07ae05866c1
Omar Omar <oomar@umich.edu>
2023-05-21 21:06:10 -0700

    Add image store handler
    - Stores handled images to a given directory
    - Add clean up

--- src/server.py
+++ src/server.py
@@ -5,8 +5,12 @@
 import argparse
 import logging
 import pickle
+import queue
+import signal
 from concurrent import futures
-from typing import List, Tuple
+from datetime import datetime
+from threading import Thread
+from typing import List, Optional, Tuple
 
 import cv2
 import grpc
@@ -22,16 +26,30 @@ from common import IMAGE_HEIGHT, IMAGE_WIDTH, LABELS_MP
 
 # Server configs.
 PORT=6969
+IMAGE_WRITER_TIMEOUT = 2
 
 # Configure logging.
 logging.basicConfig(level=logging.INFO)
 
+from dataclasses import dataclass
+
+
+@dataclass
+class ClassifiedImage:
+    image: numpy.ndarray
+    name: str
 
 class ImageClassifierServer(image_classification_pb2_grpc.ImageClassifierServicer):
     model: cv2.dnn.Net
     threshold: int
 
-    def __init__(self, threshold: int) -> None:
+    # Image store settings.
+    image_store_active: bool
+    image_store_t: Optional[Thread]
+    image_store_queue: queue.Queue[ClassifiedImage]
+    image_store_dir: str
+
+    def __init__(self, threshold: int, image_store_dir: Optional[str] = None) -> None:
         # Load & configure the DNN model.
         logging.debug(f"Loading model from {MODEL_CONFIG_PATH} | {MODEL_WEIGHTS_PATH}")
         self.model = cv2.dnn_DetectionModel(MODEL_WEIGHTS_PATH, MODEL_CONFIG_PATH)
@@ -42,7 +60,38 @@ class ImageClassifierServer(image_classification_pb2_grpc.ImageClassifierService
 
         self.threshold = threshold
 
+        # Spin up a thread to consume images to be stored in a given directory.
+        self.image_store_queue = queue.Queue(2048)
+        if image_store_dir:
+            self.image_store_active = True
+            self.image_store_dir = image_store_dir
+            self.image_store_t = Thread(target=self.image_writer_thread)
+            self.image_store_t.start()
+            self.setup_sigint()
+        else:
+            self.image_store_active = False
+
+
         super().__init__()
+
+    def image_writer_thread(self):
+        """
+            Function intended to run within a thread, which handles queued up images ready to
+            be written to a file in a directory.
+        """
+        logging.info('Starting up image writer thread')
+        while self.image_store_active:
+            # Consume image from queue with a timeout in order to check closing this thread.
+            try:
+                image = self.image_store_queue.get(timeout=IMAGE_WRITER_TIMEOUT)
+            except queue.Empty:
+                logging.debug('Image store thread timed out: Image queue empty')
+                continue
+
+            now = datetime.now()
+            filename = f'{now}-{image.name}.jpg'
+            cv2.imwrite(f'{self.image_store_dir}/{filename}', image.image)
+        logging.info('Shutting down image writer thread')
 
     @staticmethod
     def detect(frame: numpy.ndarray, model: cv2.dnn_DetectionModel, score_thresh: int) -> Tuple[List[int], List[float]]:
@@ -93,7 +142,7 @@ class ImageClassifierServer(image_classification_pb2_grpc.ImageClassifierService
             logging.debug(f'Recieved request for {request.device}. Classifying...')
 
             # Extract the image into an ndarray which the model would understand.
-            frame = pickle.loads(request.image)
+            frame: numpy.ndarray = pickle.loads(request.image)
 
             # Classify the image.
             matches, scores = self.detect(frame, self.model, self.threshold)
@@ -102,6 +151,13 @@ class ImageClassifierServer(image_classification_pb2_grpc.ImageClassifierService
             # Encode the new frame with a potentially outlined match(es).
             encoded_frame = pickle.dumps(frame)
 
+            # Offload storing the classified image to a thread, if one's active.
+            if self.image_store_active:
+                self.image_store_queue.put(ClassifiedImage(
+                    image=frame,
+                    name=request.device,
+                ))
+
             # Return result.
             yield image_classification_pb2.ClassifyImageResponse(
                 matches=list(matches),
@@ -109,6 +165,19 @@ class ImageClassifierServer(image_classification_pb2_grpc.ImageClassifierService
                 device=request.device,
                 match_scores=list(scores),
             )
+
+    def setup_sigint(self):
+        """Sets up SIGINT signal to clean things up."""
+        def _handler(s, f):
+            self.stop()
+        signal.signal(signal.SIGINT, _handler)
+
+    def stop(self):
+        """Clean up."""
+        if self.image_store_active and self.image_store_t:
+            self.image_store_active = False
+            self.image_store_t.join()
+        exit(0)
 
 
 def range_limited_float_type(min: float, max: float):
@@ -132,6 +201,12 @@ def parse_args() -> argparse.Namespace:
         default=0.2,
         help='Matching score threshold'
     )
+    parser.add_argument(
+        '--image_store_dir',
+        type=str,
+        default=None,
+        help='Path to a directory in which to store images to'
+    )
     return parser.parse_args()
 
 def main():
@@ -146,6 +221,7 @@ def main():
     )
     image_classification_pb2_grpc.add_ImageClassifierServicer_to_server(ImageClassifierServer(
         threshold=args.threshold,
+        image_store_dir=args.image_store_dir,
     ), server)
     server.add_insecure_port(f'[::]:{PORT}')
     print(f'Starting server on :{PORT}')

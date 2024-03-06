6804a394d5169105d0225a0653abc8152eb13409
Omar Omar <oomar@umich.edu>
2024-03-05 21:35:44 -0800

    update cam server

--- src/camera/server.rs
+++ src/camera/server.rs
@@ -1,31 +1,35 @@
 /// Implementation for hosting a tcp server to stream a video device.
+use crate::data_struct::ring_buffer::RingBuffer;
 use actix_web::http::header::ACCESS_CONTROL_ALLOW_ORIGIN;
-use actix_web::{get, web, App, Handler, HttpResponse, HttpServer, Responder};
+use actix_web::{http::header::ContentType, web, App, HttpResponse, HttpServer, Responder};
 use async_stream::stream;
-use log::{debug, error, info};
+use log::info;
 use std::convert::Infallible;
-use std::io::Write;
-use std::time::{SystemTime, UNIX_EPOCH};
-use tokio::sync::mpsc;
-
-// Multi-part constants.
-static MULTIPART_BOUNDARY: &str = "01multipart_boundry_seperation10";
-static CORS_ALLOW_ORIGIN: &str = "*";
+use std::sync::{Arc, RwLock};
 
 pub struct CameraServer {
-  camera_chan_rx: mpsc::Receiver<Vec<u8>>,
+  cam_buffer: Arc<RingBuffer<Vec<u8>>>,
 }
 
 impl CameraServer {
-  pub fn new(rx: mpsc::Receiver<Vec<u8>>) -> Self {
-    Self { camera_chan_rx: rx }
+  pub fn new() -> Self {
+    Self {
+      cam_buffer: Arc::new(RingBuffer::new(69)),
+    }
   }
 
-  pub async fn start(self, port: u16) {
+  /// Starts camera server, which passes in the RingBuffer used
+  /// to for responses.
+  ///
+  /// Args:
+  ///   - port: Port to listen on.
+  pub async fn start(&self, port: u16) {
     info!("Listening on :{port}");
-    let _ = HttpServer::new(|| {
+    let data = web::Data::new(RwLock::new(self.cam_buffer.clone()));
+
+    let _ = HttpServer::new(move || {
       App::new()
-        .app_data(web::Data::new(&self.camera_chan_rx)) // WIP: figure this out.
+        .app_data(web::Data::clone(&data))
         .route("/", web::get().to(serve))
     })
     .bind(("127.0.0.1", port))
@@ -35,53 +39,19 @@ impl CameraServer {
   }
 }
 
-async fn serve(chan: web::Data<mpsc::Receiver<Vec<u8>>>) -> impl Responder {
-  let mixed_multipart_mime = format!(
-    "{}/x-mixed-replace;boundary={}",
-    mime::MULTIPART,
-    MULTIPART_BOUNDARY
-  );
-
+async fn serve(data: web::Data<RwLock<RingBuffer<Vec<u8>>>>) -> impl Responder {
   let stream = stream! {
-    yield Ok::<_, Infallible>(web::Bytes::from("data"));
+    let mut data = data.write().unwrap();
+    loop {
+      match data.pop().await {
+        Ok(img) => yield Ok::<_, Infallible>(web::Bytes::from(img)),
+        Err(_) => todo!()
+      }
+    }
   };
 
   HttpResponse::Ok()
-    .content_type(mixed_multipart_mime)
-    .insert_header((ACCESS_CONTROL_ALLOW_ORIGIN, CORS_ALLOW_ORIGIN))
+    .content_type(ContentType::octet_stream())
+    .insert_header((ACCESS_CONTROL_ALLOW_ORIGIN, "*"))
     .streaming(stream)
 }
-
-/*
- async fn generate_data() -> Vec<u8> {
-   // Do "work"...
-   tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
-   let data: &str = "yee data";
-
-   let mut buffer: Vec<u8> = Vec::new();
-   let now = SystemTime::now();
-   let epoch_timestamp = now.duration_since(UNIX_EPOCH).unwrap();
-
-   // Write multi-part chunk.
-   buffer
-     .write(format!("\r\n--{MULTIPART_BOUNDARY}\r\n").as_bytes())
-     .unwrap();
-   buffer
-     .write(
-       format!(
-         "Content-Type: image/jpeg\r\nContent-Length: {}\r\nX-Timestamp: {}.{}\r\n\r\n",
-         data.len(),
-         epoch_timestamp.as_secs(),
-         epoch_timestamp.subsec_micros(),
-       )
-       .as_bytes(),
-     )
-     .unwrap();
-   buffer.write(data.as_bytes()).unwrap();
-
-   buffer
- }
-
-
-
-*/

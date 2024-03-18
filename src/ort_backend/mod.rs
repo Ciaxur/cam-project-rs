227f3a1822563916dcba4d036f8081cbc738659f
Omar Omar <oomar@umich.edu>
2024-03-18 03:47:36 -0700

    move logic into backend

--- src/ort_backend/mod.rs
+++ src/ort_backend/mod.rs
@@ -1,8 +1,10 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, time::Instant};
 
 use anyhow::{Error, Result};
+use image::{imageops::FilterType, DynamicImage, GenericImageView};
 use log::{error, info};
-use ort::{inputs, CPUExecutionProvider, ExecutionProvider, ROCmExecutionProvider, Session};
+use ndarray::{Array, Axis, CowArray, IxDyn};
+use ort::{inputs, CPUExecutionProvider, ExecutionProvider, ROCmExecutionProvider, Session, Value};
 use regex::Regex;
 
 pub struct YoloOrtModel {
@@ -24,8 +26,7 @@ impl YoloOrtModel {
     let cpu_provider = CPUExecutionProvider::default();
     info!("CPU Provider available? {}", cpu_provider.is_available()?);
 
-    // if rocm_provider.is_available()? {
-    if false {
+    if rocm_provider.is_available()? {
       match rocm_provider.register(&builder) {
         Ok(_) => info!("Successfuly registered ROCm provider"),
         Err(err) => {
@@ -57,6 +58,120 @@ impl YoloOrtModel {
 
   pub fn get_label(&self, key: &String) -> Option<&String> {
     self.labels.get(key)
+  }
+
+  pub fn version(&self) -> Result<String, Error> {
+    let version: String = self
+      .session
+      .metadata()?
+      .custom("version")?
+      .unwrap_or("0.0.0".to_string());
+
+    let author: String = self
+      .session
+      .metadata()?
+      .custom("author")?
+      .unwrap_or("unknown".to_string());
+
+    Ok(format!("{author} YOLO-{version}"))
+  }
+
+  fn preprocess(&self, input: &DynamicImage) -> Result<Array<f32, IxDyn>, Error> {
+    let expected_input_shape = self.session.inputs[0]
+      .input_type
+      .tensor_dimensions()
+      .unwrap();
+    info!("Expected input dimensions -> {:?}", expected_input_shape);
+
+    if let [num_imgs_dim, pixel_dim, height_dim, width_dim] = expected_input_shape[..4] {
+      // Resize the inputs to match expected by the model.
+      let xs = input.resize_exact(width_dim as u32, height_dim as u32, FilterType::CatmullRom);
+      info!("Input Image resized -> {:?}", xs.dimensions());
+
+      let mut input = Array::zeros((
+        num_imgs_dim as usize,
+        pixel_dim as usize,
+        height_dim as usize,
+        width_dim as usize,
+      ))
+      .into_dyn();
+      for pixel in xs.pixels() {
+        let x = pixel.0 as _;
+        let y = pixel.1 as _;
+        let [r, g, b, _] = pixel.2 .0;
+        input[[0, 0, y, x]] = (r as f32) / 255.;
+        input[[0, 1, y, x]] = (g as f32) / 255.;
+        input[[0, 2, y, x]] = (b as f32) / 255.;
+      }
+      Ok(input)
+    } else {
+      Err(Error::msg(format!(
+        "Expected model inputs to contain 4 dimensions but got -> {:?}",
+        expected_input_shape
+      )))
+    }
+  }
+
+  fn run_inference(&self, xs: &Array<f32, IxDyn>) -> Result<(), Error> {
+    let ys = self.session.run(inputs!["images" => xs.view()]?)?;
+
+    // TODO: understand how tf this is layed out.
+
+    // Extract results.
+    let output_name = self.session.outputs[0].name.clone();
+    let output = ys[output_name].extract_tensor::<f32>()?;
+    info!("Output -> {:?}", output);
+
+    // Draw boxes around match.
+    // let mut boxes = Vec::new();
+    for row in output
+      .view()
+      .slice(ndarray::s![.., .., 0])
+      .axis_iter(Axis(0))
+    {
+      let row: Vec<_> = row.iter().copied().collect();
+      let (class_id, prob) = row
+        .iter()
+        // skip bounding box coordinates
+        .skip(4)
+        .enumerate()
+        .map(|(index, value)| (index, *value))
+        .reduce(|acc, row| if row.1 > acc.1 { row } else { acc })
+        .unwrap();
+
+      /**
+       * WIP: continue here:
+       * https://github.com/pykeio/ort/blob/main/examples/yolov8/examples/yolov8.rs#L83
+       * https://github.com/ultralytics/ultralytics/blob/main/examples/YOLOv8-ONNXRuntime-Rust/src/ort_backend.rs
+       */
+      let label = self.get_label(&class_id.to_string()).unwrap();
+      info!("class_id={class_id} | label={label} | prob={prob}");
+    }
+
+    // TODO: make a result struct.
+    Ok(())
+  }
+
+  fn postprocess(&self) {}
+
+  pub fn run(&self, input: DynamicImage) -> Result<(), Error> {
+    // Pre-process.
+    let pre_process_dt = Instant::now();
+    let xs = self.preprocess(&input)?;
+    info!("Pre-process took {:?}", pre_process_dt.elapsed());
+
+    // Inference.
+    let inference_dt = Instant::now();
+    self.run_inference(&xs)?;
+    info!("Inference took {:?}", inference_dt.elapsed());
+
+    // Post-process.
+    // let post_process_dt = Instant::now();
+    // self.postprocess();
+    // info!("Post-process took {:?}", post_process_dt.elapsed());
+
+    // Results.
+    Ok(())
   }
 
   fn extract_labels_from_metadata(&mut self) -> Result<(), Error> {
--- src/server.rs
+++ src/server.rs
@@ -13,77 +13,12 @@ fn run_server() -> Result<(), Error> {
   let onnx_model_path = "./models/yolov8/yolov8n.onnx";
 
   let yolo_model = YoloOrtModel::new(onnx_model_path.to_string())?;
-  let session = &yolo_model.session;
+  info!("Version -> {}", yolo_model.version()?);
 
-  // Grab expected input dimensions.
-  let expected_input_shape = session.inputs[0].input_type.tensor_dimensions().unwrap();
-  if let [num_imgs_dim, pixel_dim, height_dim, width_dim] = expected_input_shape[..4] {
-    info!("Expected input dimensions -> {:?}", expected_input_shape);
-
-    // Load in the image and resize it to match expected input shape.
-    let og_img = image::open("keanu.jpg")?;
-    info!("Image loaded -> {:?}", og_img.dimensions());
-
-    let img = og_img.resize_exact(width_dim as u32, height_dim as u32, FilterType::CatmullRom);
-    info!("Image resized -> {:?}", img.dimensions());
-
-    // Let's do some inference.
-    let mut input = Array::zeros((
-      num_imgs_dim as usize,
-      pixel_dim as usize,
-      height_dim as usize,
-      width_dim as usize,
-    ));
-    for pixel in img.pixels() {
-      let x = pixel.0 as _;
-      let y = pixel.1 as _;
-      let [r, g, b, _] = pixel.2 .0;
-      input[[0, 0, y, x]] = (r as f32) / 255.;
-      input[[0, 1, y, x]] = (g as f32) / 255.;
-      input[[0, 2, y, x]] = (b as f32) / 255.;
-    }
-    let t = Instant::now();
-    let outputs = session.run(inputs!["images" => input.view()]?)?;
-    info!("Inference took {:?}", t.elapsed());
-
-    // Extract results.
-    let output_name = session.outputs[0].name.clone();
-    let output = outputs[output_name].extract_tensor::<f32>()?;
-    info!("Output -> {:?}", output);
-
-    // Draw boxes around match.
-    // let mut boxes = Vec::new();
-    for row in output
-      .view()
-      .slice(ndarray::s![.., .., 0])
-      .axis_iter(Axis(0))
-    {
-      let row: Vec<_> = row.iter().copied().collect();
-      let (class_id, prob) = row
-        .iter()
-        // skip bounding box coordinates
-        .skip(4)
-        .enumerate()
-        .map(|(index, value)| (index, *value))
-        .reduce(|acc, row| if row.1 > acc.1 { row } else { acc })
-        .unwrap();
-
-      /**
-       * WIP: continue here:
-       * https://github.com/pykeio/ort/blob/main/examples/yolov8/examples/yolov8.rs#L83
-       * https://github.com/ultralytics/ultralytics/blob/main/examples/YOLOv8-ONNXRuntime-Rust/src/ort_backend.rs
-       */
-      let label = yolo_model.get_label(&class_id.to_string()).unwrap();
-      info!("class_id={class_id} | label={label} | prob={prob}");
-    }
-
-    Ok(())
-  } else {
-    Err(Error::msg(format!(
-      "Expected model inputs to contain 4 dimensions but got -> {:?}",
-      expected_input_shape
-    )))
-  }
+  // Load in the image and resize it to match expected input shape.
+  let img = image::open("video0.jpg")?;
+  info!("Image loaded -> {:?}", img.dimensions());
+  yolo_model.run(img)
 }
 
 fn main() {
